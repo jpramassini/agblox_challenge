f5e680864fb95f256ba373c3646af6ae26e0aa5b
jpramassini <jpramassini@gmail.com>
2019-04-06 23:38:29 -0400

    Implemented more dynamic version, now should react to state changes more effectively.

--- agblox_hw/src/visualizations/lineGraph.js
+++ agblox_hw/src/visualizations/lineGraph.js
@@ -1,85 +1,68 @@
 import React, { Component } from "react";
 import * as d3 from "d3";
 
-const svgWidth = 1200;
-const svgHeight = 1000;
-const margin = { top: 20, right: 20, bottom: 30, left: 50 };
-const width = svgWidth - (margin.left + margin.right);
-const height = svgHeight - (margin.top + margin.bottom);
+const width = 650;
+const height = 400;
+const margin = { top: 20, right: 5, bottom: 20, left: 35 };
+const red = "#eb6a5b";
+const blue = "#52b6ca";
 
 class LineGraph extends Component {
-  constructor(props) {
-    super(props);
-    this.svg = React.createRef();
-    this.xAxis = React.createRef();
-    this.yAxis = React.createRef();
-  }
+  state = {
+    // svg path command for graph
+    values: null,
+    // d3 helpers
+    xScale: d3.scaleTime().range([margin.left, width - margin.right]),
+    yScale: d3.scaleLinear().range([height - margin.bottom, margin.top]),
+    lineGenerator: d3.line()
+  };
 
-  drawLineGraph() {
-    let x = d3.scaleTime().range([0, width]);
-    let y = d3.scaleLinear().range([height, 0]);
-    console.log(
-      d3.extent(this.props.data, d => {
-        return d.ts;
-      })
-    );
-    let xScale = d3.scaleTime().range([margin.left, width - margin.right]);
-    let yScale = d3.scaleLinear().range([height - margin.bottom, margin.top]);
-    if (this.props.data) {
-      console.log(d3.extent(this.props.data, d => d.rel_speed));
-      const timeDomain = d3.extent(this.props.data, d => d.ts);
-      const max = d3.max(this.props.data, d => d.rel_speed);
-      xScale.domain(timeDomain);
-      yScale.domain([0, max]);
-    }
-    let xAxis = d3
-      .axisBottom()
-      .scale(xScale)
-      .tickFormat("%Y-%m-%d %H");
-    let yAxis = d3
-      .axisLeft()
-      .scale(yScale)
-      .tickFormat(d => d);
+  xAxis = d3
+    .axisBottom()
+    .scale(this.state.xScale)
+    .tickFormat(d3.timeFormat("%Y-%m-%d %H"));
+  yAxis = d3
+    .axisLeft()
+    .scale(this.state.yScale)
+    .tickFormat(d => `${d}`);
 
-    let line = d3
-      .line()
-      .x(d => xScale(d.ts))
-      .y(d => yScale(d.rel_speed))
-      .curve(d3.curveBasis);
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (!nextProps.data) return null; // data hasn't been loaded yet so do nothing
+    const { data } = nextProps;
+    const { xScale, yScale, lineGenerator } = prevState;
 
-    return (
-      <path
-        fill="none"
-        stroke="#282c34"
-        strokeWidth="1"
-        d={line(this.props.data)}
-      />
-    );
+    // data has changed, so recalculate scale domains
+    const timeDomain = d3.extent(data, d => d.ts);
+    const tempMax = d3.max(data, d => d.rel_speed);
+    xScale.domain(timeDomain);
+    yScale.domain([0, tempMax]);
+
+    // calculate line for value
+    lineGenerator.x(d => xScale(d.ts));
+    lineGenerator.y(d => yScale(d.rel_speed));
+    lineGenerator.curve(d3.curveBasis);
+    const values = lineGenerator(data);
+    return { values };
   }
 
   componentDidUpdate() {
-    /*d3.select(this.refs.xAxis).call(this.xAxis);
-    d3.select(this.refs.yAxis).call(this.yAxis);*/
+    d3.select(this.refs.xAxis).call(this.xAxis);
+    d3.select(this.refs.yAxis).call(this.yAxis);
   }
 
   render() {
-    if (this.props.data) {
-      return (
-        <svg zindex={1000} ref={this.svg} height={height} width={width}>
-          {this.drawLineGraph()}
+    return (
+      <svg width={width} height={height}>
+        <path d={this.state.values} fill="none" stroke={red} strokeWidth="2" />
+        <g>
+          <g
+            ref="xAxis"
+            transform={`translate(0, ${height - margin.bottom})`}
           />
-          <g>
-            <g
-              ref="xAxis"
-              transform={`translate(0, ${height - margin.bottom})`}
-            />
-            <g ref="yAxis" transform={`translate(${margin.left}, 0)`} />
-          </g>
-        </svg>
-      );
-    } else {
-      return <div />;
-    }
+          <g ref="yAxis" transform={`translate(${margin.left}, 0)`} />
+        </g>
+      </svg>
+    );
   }
 }
 
