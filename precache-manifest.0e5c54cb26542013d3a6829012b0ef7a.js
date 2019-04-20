f24578ddc3ca38d98e3bbd9057da99875de664c1
jpramassini <jpramassini@gmail.com>
2019-04-20 16:42:47 -0400

    Updates

---
+++ precache-manifest.0e5c54cb26542013d3a6829012b0ef7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdd0e8e4278a265eaccc6ed0b5579720",
+    "url": "/agblox_challenge/static/media/data_sample.fdd0e8e4.csv"
+  },
+  {
+    "revision": "1e9e6e08f4b74344fd17",
+    "url": "/agblox_challenge/static/js/runtime~main.7fcffeec.js"
+  },
+  {
+    "revision": "500a3bbf755dfa4088f9",
+    "url": "/agblox_challenge/static/js/main.f1586c50.chunk.js"
+  },
+  {
+    "revision": "4e67f925b9d13edf2076",
+    "url": "/agblox_challenge/static/js/2.39829238.chunk.js"
+  },
+  {
+    "revision": "500a3bbf755dfa4088f9",
+    "url": "/agblox_challenge/static/css/main.95b1ef4f.chunk.css"
+  },
+  {
+    "revision": "4e67f925b9d13edf2076",
+    "url": "/agblox_challenge/static/css/2.0f6b40c4.chunk.css"
+  },
+  {
+    "revision": "89d3fb54341586ffe83ffc2ddb4eae2c",
+    "url": "/agblox_challenge/index.html"
+  }
+];
